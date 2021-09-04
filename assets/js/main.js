3ffeb2503d2885fea1d8a16d9a17ce6d524db833
Minh Hiền <51699754+hiennm11@users.noreply.github.com>
2021-09-04 16:22:40 +0700

    update responsive css + modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
+var header = document.getElementById('header');
+var currentHeight = header.clientHeight;
+
 var btns = document.querySelectorAll('.js-buy-ticket');
 var modal = document.querySelector('.modal');
 var modalCloseBtns = document.querySelectorAll('.js-close-btn');
 var modalContent = document.querySelector('.modal-content');
+var mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+
 
 btns.forEach(btn => {
     btn.addEventListener('click', openModal);
@@ -15,6 +20,33 @@ modal.addEventListener('click', closeModal);
 
 modalContent.addEventListener('click', event => {
     event.stopPropagation();
+})
+
+
+mobileMenuBtn.onclick = function() {
+    var isClose = header.clientHeight === currentHeight;
+    if(isClose){
+        header.style.height = 'auto';
+    }
+    else{
+        header.style.height = null;
+    }
+}
+
+
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+menuItems.forEach(item => {
+    var isParentMenu = item.nextElementSibling && item.nextElementSibling.classList.contains('subnav');
+    
+    item.onclick = function(event) {
+        if(isParentMenu)
+        {
+            event.preventDefault();
+        }     
+        else{
+            header.style.height = null;
+        }   
+    };
 })
 
 function openModal()
